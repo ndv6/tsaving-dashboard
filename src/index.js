f0a5a06ea75db1d1b318281d5d09277610a5fa12
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-08-05 17:45:28 +0700

    add table components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import * as serviceWorker from "./serviceWorker";
 import Routes from "./components/Routes";
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
