ef10494345dd41a8da1d7c431d3fb4547ab47665
sayaandreas <sayaandreas@gmail.com>
2020-08-06 11:01:06 +0700

    add credit card component & import antd.css to App.css

---
+++ src/components/DebitCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Row, Col, Typography } from 'antd';
+import './DebitCard.css';
+import chip from '../static/chip.png';
+import master from '../static/master.svg';
+
+const { Title, Text } = Typography;
+
+export default function DebitCard(props) {
+    return (
+        <div className="container">
+            <Row>
+                <Col>
+                    <Title style={{ color: 'white', fontFamily: 'serif' }}>BAIT BANK</Title>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <img className="image-logo" alt="chip" src={chip}></img>
+                </Col>
+            </Row>
+            <Row style={{ marginTop: 10 }}>
+                <Col>
+                    <Title level={2} style={{ color: 'white', fontFamily: 'monospace' }}>{props.cardNum}</Title>
+                </Col>
+            </Row>
+            <Row justify="center">
+                <Col>
+                    <Text className="valid" style={{ color: 'grey', fontFamily: 'serif', fontSize: 12 }}>VALID THRU</Text>
+                    <Text style={{ color: 'white', fontFamily: 'monospace' }}> {props.validThru}</Text>
+                </Col>
+            </Row>
+            <Row justify="space-between" align="middle">
+                <Col>
+                    <Title level={3} style={{ color: 'white', fontFamily: 'monospace' }}>{props.name}</Title>
+                </Col>
+                <Col>
+                    <img className="image-logo" alt="master-logo" src={master}></img>
+                </Col>
+            </Row>
+        </div>
+    );
+}
