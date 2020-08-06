a60695665c9d794ee1112320921bfa2d4abda9fe
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-08-06 09:08:37 +0700

    add tab component

---
+++ src/components/Tab.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+export default function Tab(props) {
+  return (
+    <div>
+      <Tabs
+        defaultActiveKey="1"
+        centered
+        size={props.size}
+        tabPosition={props.position}
+        type={props.type}
+      >
+        {props.tabs.map((value, index) => {
+          return (
+            <TabPane tab={value.tabname} key={index}>
+              {value.components}
+            </TabPane>
+          );
+        })}
+      </Tabs>
+    </div>
+  );
+}
