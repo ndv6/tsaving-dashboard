b5c25d553a884249bb4a7d9315e4ab8458d3f4c0
David Bernadi <davidbernadi13@gmail.com>
2020-08-06 16:20:53 +0700

    wip local

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
-import TransactionLog from "./TransactionLog";
+import TransactionLog from "./TransactionLog"
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/admin/transaction-log">
+          <TransactionLog />
+        </Route>
         <Route path="*">
           <PageNotFound />
-        </Route>
-        <Route path="/admin/transactionlog">
-          <TransactionLog />
         </Route>
       </Switch>
     </Router>
--- src/pages/TransactionLog.js
+++ src/pages/TransactionLog.js
@@ -1,9 +1,88 @@
-import React from "react";
+import React, {useState} from 'react';
+
+import DataTable from '../components/DataTable'
+import '../styles/TransactionLog.css'
 
 export default function TransactionLog() {
-  return (
-    <div>
-      <h1>Oops!</h1>
-      <p>404 Page Not Found</p>
-    </div>
-  );
+    const [collapse, setCollapse] = useState(false);
+ 
+    function onToogle(){
+        setCollapse(!collapse);
+    };
+    //prepare column
+
+    const columns = [
+        {
+            title: 'Acc Number',
+            dataIndex: 'accnum'
+        },
+        {
+            title: 'From',
+            dataIndex: 'from'
+        },
+        {
+            title: 'To',
+            dataIndex: 'to'
+        },
+        {
+            title: 'Desc',
+            dataIndex: 'from'
+        },
+        {
+            title: 'Amount',
+            dataIndex: 'amount'
+        },
+        {
+            title: 'Date',
+            dataIndex: 'date'
+        }
+
+    ];
+    const dataSource = [
+        {   
+            key: '1',
+            accnum : "8220912312",
+            from : "main acc",
+            to : "882727282",
+            from: "asdsad",
+            amount: "87.000",
+            date: "19-10-2020 08:00"
+            
+        },
+        // {
+        //     key: '2',
+        //     accnum : "8220912312",
+        //     from : "va acc"
+        // },
+        // {
+        //     key: '3',
+        //     accnum : "8220912312",
+        //     from : "va acc"
+        // },
+        // {
+        //     key: '4',
+        //     accnum : "8220912312",
+        //     from : "va acc"
+        // },
+        // {
+        //     key: '5',
+        //     accnum : "8220912312",
+        //     from : "va acc"
+        // },
+        
+    ]
+    
+    
+    // const loading = "<div>Loading ... </div>";
+
+
+    return (    
+        <div className="content-table-tl">
+            <div className="tl-title">Transaction List :</div>
+            <button>filter</button>
+            <div className="table-tl-list">
+                <DataTable columns={columns} data={dataSource} pagePosition="bottomRight"/>
+            </div>   
+        </div>
+    );
+  }
