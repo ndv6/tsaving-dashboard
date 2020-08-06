4df0a9b726904211ef2b79bbd84ba287466d8b1e
sayaandreas <sayaandreas@gmail.com>
2020-08-06 17:06:37 +0700

    remove inline style

--- src/components/DebitCard.js
+++ src/components/DebitCard.js
@@ -11,7 +11,9 @@ export default function DebitCard(props) {
         <div className="card-container">
             <Row>
                 <Col>
-                    <Title style={{ color: 'white', fontFamily: 'serif' }}>BAIT BANK</Title>
+                    <div className="title">
+                        <Title>BAIT BANK</Title>
+                    </div>
                 </Col>
             </Row>
             <Row>
@@ -19,20 +21,24 @@ export default function DebitCard(props) {
                     <img className="image-logo" alt="chip" src={chip}></img>
                 </Col>
             </Row>
-            <Row style={{ marginTop: 10 }}>
+            <Row>
                 <Col>
-                    <Title level={2} style={{ color: 'white', fontFamily: 'monospace' }}>{props.cardNum}</Title>
+                    <div className="title">
+                        <Title level={2} >{props.cardNum}</Title>
+                    </div>
                 </Col>
             </Row>
             <Row justify="center">
                 <Col>
-                    <Text className="valid" style={{ color: 'grey', fontFamily: 'serif', fontSize: 12 }}>VALID THRU</Text>
-                    <Text style={{ color: 'white', fontFamily: 'monospace' }}> {props.validThru}</Text>
+                    <Text className="validLabel" >VALID THRU</Text>
+                    <Text className="validThru"> {props.validThru}</Text>
                 </Col>
             </Row>
             <Row justify="space-between" align="middle">
                 <Col>
-                    <Title level={3} style={{ color: 'white', fontFamily: 'monospace' }}>{props.name}</Title>
+                    <div className="title">
+                        <Title level={3} >{props.name}</Title>
+                    </div>
                 </Col>
                 <Col>
                     <img className="image-logo" alt="master-logo" src={master}></img>
