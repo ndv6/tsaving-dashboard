702fb368004bbb3ea0947998a6869eebefde992e
David Bernadi <davidbernadi13@gmail.com>
2020-08-07 10:52:28 +0700

    wip local

--- src/pages/TransactionLog.js
+++ src/pages/TransactionLog.js
@@ -4,38 +4,67 @@ import NavigationBar from '../components/NavigationBar'
 import DataTable from '../components/DataTable'
 import '../styles/TransactionLog.css'
 
-export default function TransactionLog() {
-    const [collapse, setCollapse] = useState(false);
- 
-    function onToogle(){
-        setCollapse(!collapse);
-    };
-    //prepare column
+import axios from 'axios';
+import { Table } from 'antd';
 
+function getTransactionLog(setListTL){
+    //catch untuk nge throw error. kalau success bakal ke then.
+    console.log("tes")
+    axios({
+      method : "GET",
+      url : "http://localhost:8000/admin/transactions"
+    }).then((res) => {
+      //success
+      console.log(res.data)
+    //   console.log(res.data.status,"data table")
+    setListTL(res.data.data)
+    }).catch((err) => {
+      //failed
+      console.log(err,'error')
+    }).finally(() => {
+      console.log("finally")
+      // setLoading(false);
+    })
+  }
+export default function TransactionLog() {
+    const [listTL,setListTL] = useState([]);
+
+    //prepare column
     const columns = [
         {
+            title: 'id',
+            dataIndex: 'tl_id',
+            key: 'id'
+        },
+        {
             title: 'Acc Number',
-            dataIndex: 'accnum'
+            dataIndex: 'account_num',
+            key: 'accnum'
         },
         {
             title: 'From',
-            dataIndex: 'from'
+            dataIndex: 'from_account',
+            key: 'from'
         },
         {
             title: 'To',
-            dataIndex: 'to'
+            dataIndex: 'dest_account',
+            key: 'to'
         },
         {
             title: 'Desc',
-            dataIndex: 'from'
+            dataIndex: 'tran_amount',
+            key: 'desc'
         },
         {
             title: 'Amount',
-            dataIndex: 'amount'
+            dataIndex: 'description',
+            key: 'amount'
         },
         {
             title: 'Date',
-            dataIndex: 'date'
+            dataIndex: 'created_at',
+            key: 'date'
         }
 
     ];
@@ -45,49 +74,42 @@ export default function TransactionLog() {
             accnum : "8220912312",
             from : "main acc",
             to : "882727282",
-            from: "asdsad",
+            desc: "asdsad",
             amount: "87.000",
             date: "19-10-2020 08:00"
-            
-        },
-        // {
-        //     key: '2',
-        //     accnum : "8220912312",
-        //     from : "va acc"
-        // },
-        // {
-        //     key: '3',
-        //     accnum : "8220912312",
-        //     from : "va acc"
-        // },
-        // {
-        //     key: '4',
-        //     accnum : "8220912312",
-        //     from : "va acc"
-        // },
-        // {
-        //     key: '5',
-        //     accnum : "8220912312",
-        //     from : "va acc"
-        // },
+        },        
+    ];
+
+    React.useEffect(() => {
+        getTransactionLog(setListTL)
+        console.log(listTL);
+      },[])
+
+    return ( 
+        // <div className="content-table-tl">
+        //     <div className="tl-title">Transaction List :</div>
+        //     <button>filter</button>
+        //     <div className="table-tl-list">
+        //         <DataTable 
+        //         columns={columns} 
+        //         data={listTL} 
+        //         size="middle"
+        //         pagePosition="bottomRight"/>
+        //     </div>   
+        // </div>   
         
-    ]
-    
-    
-    // const loading = "<div>Loading ... </div>";
-
-
-    return (    
         <div className="transaction-log-constraint">
             <NavigationBar></NavigationBar>
             <div className="transaction-log-content">
-                    <div className="content-table-tl">
-                    <div className="tl-title">Transaction List :</div>
-                    <button>filter</button>
-                    <div className="table-tl-list">
-                        <DataTable columns={columns} data={dataSource} pagePosition="bottomRight"/>
-                    </div>   
-                </div>
+                <div className="tl-title">Transaction List :</div>
+                <button>filter</button>
+                <div className="table-tl-list">
+                    <Table 
+                    columns={columns} 
+                    data={dataSource} 
+                    size="middle"
+                    pagePosition="bottomRight"/>
+                </div>  
             </div>
         </div>
 
