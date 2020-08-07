bf1456fbd1e35c98c0a994751d4434ef689fd69c
Yuly Haruka Berliana Gunawan <yulyharuka@gmail.com>
2020-08-07 18:35:02 +0700

    delete testing from router

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -6,35 +6,11 @@ import Customers from "./Customers";
 import TransactionLog from "./TransactionLog";
 import ActivityLog from "./ActivityLog";
 import PageNotFound from "./PageNotFound";
-import CustomerTransactionLog from "./CustomerTransactionLog";
-import Tabs from '../components/Tab'
-
-const tabs = [
-  {
-    tabname: 'Profile',
-    components: <PageNotFound />,
-    key: '1',
-  },
-  {
-    tabname: 'Va List',
-    components: <PageNotFound />,
-    key: '2',
-  },
-  {
-    tabname: 'Transaction History',
-    components: <CustomerTransactionLog />,
-    key: '3',
-  },
-];
-
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/admin">
-          <Tabs size="medium" tabs={tabs}/>
-        </Route>
         <Route exact path="/admin/login">
           <Login />
         </Route>
