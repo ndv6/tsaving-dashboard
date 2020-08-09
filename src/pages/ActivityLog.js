4a45ba66264a9f06455de34e15a4b3745773ad18
jocelyntjahyadi <jocelyntjahyadi26@gmail.com>
2020-08-09 23:14:59 +0700

    admin log

--- src/pages/ActivityLog.js
+++ src/pages/ActivityLog.js
@@ -4,26 +4,54 @@ import "../styles/ActivityLog.css"
 import axios from 'axios';
 import NavigationBar from "../components/NavigationBar"
 import FilterBar from "../components/FilterBar"
-import { Pagination,Space } from 'antd';
+import { Input } from 'antd';
 
+const { Search } = Input;
 
-function filter(value){
-  console.log(value)
-}
-
-function getActivityLog(setList,pageNumber,setTotaldata){
+function getActivityLog(token, setList,pageNumber,setTotaldata,date,username){
     //catch untuk nge throw error. kalau success bakal ke then.
+    let url ="";
+    let modifDate = "";
+    if(date != null){
+       let dateFormat = date.split("-");
+       modifDate = dateFormat[0] + "-" + ('0'+dateFormat[1]).slice(-2) +"-" + ('0'+dateFormat[2]).slice(-2);
+    }
+    
+    if(date == null && username == null){
+      url = "http://localhost:8000/v2/log/"+pageNumber
+    }else if(date != null && username == null){
+      url = "http://localhost:8000/v2/log/d/"+modifDate+"/"+pageNumber;
+    }else if(date == null && username != null){
+      url = "http://localhost:8000/v2/log/u/"+username+"/"+pageNumber;
+    }else if(date != null && username != null){
+      url = "http://localhost:8000/v2/log/"+username+"/"+modifDate +"/" + pageNumber;
+    }
+
+    console.log(url);
     
     console.log("tes")
     axios({
-      method : "GET",
-      url : "http://localhost:8000/v2/log/"+pageNumber+"",
       headers:{
-        authorization: window.localStorage.getItem("token")
-      }
+        'Content-Type': "application/json",
+        "Authorization" : token,
+      },
+      method : "GET",
+      url : url,
+      
     }).then((res) => {
       //success
-      setList(res.data.data.list)
+      const tableData = (res.data.data.list || []).map((value, index) => {
+        let singleRow = {};
+
+        singleRow["key"] = index;
+        singleRow["no"] = ((pageNumber-1)*20) + (index + 1) ;
+        singleRow["username"] = value.username;
+        singleRow["acc_num"] = value.acc_num;
+        singleRow["action_time"] = new Date(value.action_time).toUTCString()
+        singleRow["action"] = value.action;
+        return singleRow
+      })
+      setList(tableData)
       setTotaldata(res.data.data.count)
     }).catch((err) => {
       //failed
@@ -34,28 +62,30 @@ function getActivityLog(setList,pageNumber,setTotaldata){
     })
   }
 
-
-
 export default function ActivityLog(){
   const [list,setList] = useState([]);
   const [page,setPage] = useState(1);
   const [totalData,setTotaldata] = useState(0);
+  const [date,setDate] = useState(null);
+  const [search,setSearch] = useState(null);
+  const token = window.localStorage.getItem("token")
 
-  const columns = [
+    const columns = [
     {
       title: 'No',
-      key: 'name',
-      render : (text, record, index) => index+page,
+      dataIndex: 'no',
+      key: 'no',
+
     },
     {
       title: 'Username',
       dataIndex: 'username',
-      key: 'age',
+      key: 'username',
     },
     {
       title: 'Account Number',
       dataIndex: 'acc_num',
-      key: 'address',
+      key: 'acc_num',
     },
     {
       title: 'Time',
@@ -66,49 +96,68 @@ export default function ActivityLog(){
     {
       title: 'Action',
       dataIndex: 'action',
-      key: 'address',
+      key: 'action',
+      render : (text) => {
+        if(text.toUpperCase() == "EDIT"){
+          return (<div className = "edit-label">{text}</div>)
+        }else if(text.toUpperCase() == "DELETE"){
+          return  (<div className = "delete-label">{text}</div>)
+        }else if(text.toUpperCase() == "RESEND"){
+          return  (<div className = "resend-label">{text}</div>)
+        }
+        return
+      }
     },
 
   ];
 
 
-    function onChange(pageNumber) {
-      console.log('Page: ', pageNumber);
-      getActivityLog(setList,pageNumber,setTotaldata)
-      var index = (pageNumber - 1) * 20 + 1;
-      setPage(index)
+  function dateChange(date){
+    if (date != null) {
+      let day = date.date().toString();
+      let month = (date.month() + 1).toString();
+      let year = date.year().toString();
+      let fixdate = year + "-" + month + "-" + day;
+      setDate(fixdate);
     }
+    else{
+        setDate(null);
+    }
+  }
 
-    React.useEffect(() => {
-      getActivityLog(setList,1)
-    },[])
+  function onChange(pageNumber) {
+    console.log(pageNumber)
+    setPage(pageNumber)
+  }
+
+  React.useEffect(() => {
+    getActivityLog(token, setList,page,setTotaldata,date,search)
+  },[setList,date,page,search])
 
     return(
         <div className = "activity-log-constraint">
-           <NavigationBar></NavigationBar>
+           <NavigationBar/>
            <div className = "activity-log-content">
               <h1>Activity Log</h1>
               <div className = "activity-log-filterbar">
-                <span className = "filter-text">Filter by date : &ensp;</span> <FilterBar></FilterBar>
+                <span className = "filter-text">Filter by date : &ensp;</span> 
+                <FilterBar onChange = {(date) => dateChange(date)}/>
+                <Search
+                  placeholder="username"
+                  onSearch={value => setSearch(value)}
+                  style={{ width: 200 }}/>
               </div>
               <div className = "table">
-
                   <DataTable 
+                    current={page}
                     columns={columns} 
                     data={list} 
                     pagePosition="bottomRight" 
                     pageSize={20} 
                     totalData={totalData} 
-                    onPageChange={(page) => onChange(page)}
-                    />
-
-                  
+                    onPageChange={(page) => onChange(page)}/>
                 </div>
-                
-                
-           </div>
-        </div>
-       
-                 
+            </div>
+        </div>   
     )
 }
