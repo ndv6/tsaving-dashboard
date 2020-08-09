c73b7a0a36d66c09c37e8d491a85a4a56831f2bd
vicimilenia <vici.ocbcnisp@gmail.com>
2020-08-09 19:20:12 +0700

    feat: add confirmation before closing edit profile modal

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -7,6 +7,13 @@ export const PRIMARY = "primary";
 export const BUTTON_BACK = "back";
 export const BUTTON_SUBMIT = "submit";
 
+// Messages
+export const PHONE_NUMBER_REQUIRED = "Phone Number is required!";
+export const INVALID_EMAIL = "Please enter a valid E-mail!";
+export const EMAIL_IS_REQUIRED = "E-mail is required!";
+export const QUIT_MODAL_CONFIRM =
+  "Are you sure you want to quit? Your changes will not be saved.";
+
 // Form Fields
 export const FORM_FIELDS = {
   EMAIL: "cust_email",
@@ -14,6 +21,14 @@ export const FORM_FIELDS = {
   ACCOUNT_NUM: "account_num",
   IS_VERIFIED: "is_verified",
   PHONE_NUMBER: "cust_phone",
+};
+
+export const FORM_LABELS = {
+  EMAIL: "Email",
+  NAME: "Customer Name",
+  ACCOUNT_NUM: "Account Number",
+  IS_VERIFIED: "Verified",
+  PHONE_NUMBER: "Phone Number",
 };
 
 // Component Titles
