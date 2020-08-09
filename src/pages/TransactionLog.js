e09cac840ee63e8118105a61efef5182973ae0ba
David Bernadi <davidbernadi13@gmail.com>
2020-08-09 13:59:11 +0700

    wip local kesekian

--- src/pages/TransactionLog.js
+++ src/pages/TransactionLog.js
@@ -1,31 +1,88 @@
 import React, {useState} from 'react';
+import { useHistory } from "react-router-dom";
 
-import NavigationBar from '../components/NavigationBar'
-import DataTable from '../components/DataTable'
-import '../styles/TransactionLog.css'
+import NavigationBar from '../components/NavigationBar';
+import DataTable from '../components/DataTable';
+import FilterBar from '../components/FilterBar';
+import SearchBar from '../components/SearchBar';
+
+import '../styles/TransactionLog.css';
 
 import axios from 'axios';
-import { Table } from 'antd';
 
-function getTransactionLog(setListTL){
-    //catch untuk nge throw error. kalau success bakal ke then.
-    console.log("tes")
+const columns = [
+    {
+        title: 'Acc Number',
+        dataIndex: 'account_num',
+        key : 'account_num'
+    },
+    {
+        title: 'From',
+        dataIndex: 'from_account',
+        key : 'from_account'
+    },
+    {
+        title: 'To',
+        dataIndex: 'dest_account',
+        key : 'dest_account'
+    },
+    {
+        title: 'Amount',
+        dataIndex: 'tran_amount',
+        key : 'tran_amount'
+    },
+    {
+        title: 'Description',
+        dataIndex: 'description',
+        key : 'description'
+    },
+    {
+        title: 'Date',
+        dataIndex: 'created_at',
+        key: 'created_at'
+    }
+];
+
+
+function getTransactionLog(paramPage=1, paramDate='', paramSearch='', setListTL, setCountData, setLoading, history){
+    setLoading(true);
     axios({
-      method : "GET",
-      url : "http://localhost:8000/admin/transactions"
+        headers: {
+            'Content-Type': "application/json",
+            "Authorization" : window.localStorage.getItem("token"),
+        },
+        method : "GET",
+        url : "http://localhost:8000/v2/transactions/list/" + paramPage
     }).then((res) => {
-      //success
-      console.log(res.data)
-    //   console.log(res.data.status,"data table")
-    setListTL(res.data.data)
+        const tableData = (res.data.data.list || []).map((value, index) => {
+            let singleRow = {};
+            const formatter = new Intl.NumberFormat('id', {
+                style: 'currency',
+                currency: 'IDR',
+                minimumFractionDigits: 2
+            });
+
+            singleRow['key'] = index;
+            singleRow['account_num'] = value.account_num;
+            singleRow['from_account'] = value.from_account;
+            singleRow['dest_account'] = value.dest_account;
+            singleRow['tran_amount'] =  formatter.format(value.tran_amount);
+            singleRow['description'] = value.description;
+            singleRow['created_at'] = new Date(value.created_at).toUTCString();
+            return singleRow;
+        })
+        setCountData(res.data.data.count);
+        setListTL(tableData);
     }).catch((err) => {
-      //failed
-      console.log(err,'error')
+        if (err.response.status === 401) {
+            localStorage.removeItem("token");
+            history.push("/admin/login")
+        }
     }).finally(() => {
-      console.log("finally")
-      // setLoading(false);
+        setLoading(false);
     })
-  }
+}
+
 export default function TransactionLog() {
     const [listTL,setListTL] = useState([]);
     const [countData, setCountData] = useState(0);
@@ -33,76 +90,55 @@ export default function TransactionLog() {
     const [paramDate, setDate] = useState(null);
     const [paramSearch, setSearch] = useState("");
     const [paramPage, setPage] = useState(1);
+    const history = useHistory();
 
-    //prepare column
-    const columns = [
-        {
-            title: 'id',
-            dataIndex: 'tl_id',
-            key: 'id'
-        },
-        {
-            title: 'Acc Number',
-            dataIndex: 'account_num',
-            key: 'accnum'
-        },
-        {
-            title: 'From',
-            dataIndex: 'from_account',
-            key: 'from'
-        },
-        {
-            title: 'To',
-            dataIndex: 'dest_account',
-            key: 'to'
-        },
-        {
-            title: 'Desc',
-            dataIndex: 'tran_amount',
-            key: 'desc'
-        },
-        {
-            title: 'Amount',
-            dataIndex: 'description',
-            key: 'amount'
-        },
-        {
-            title: 'Date',
-            dataIndex: 'created_at',
-            key: 'date'
+    function pageChange(page){
+        setPage(page);
+    }
+    function filterDate(date) {
+        if (date !== null) {
+          let day = date.date().toString();
+          let month = (date.month() + 1).toString();
+          let year = date.year().toString();
+          let fixdate = year + "-" + month + "-" + day;
+          setDate(fixdate);
         }
-
-    ];
+        else{
+            setDate("");
+        }
+    }
+    function searchTL(value){
+        setSearch(value);
+    }
 
     React.useEffect(() => {
-        getTransactionLog(setListTL)
-        console.log(listTL);
-      },[setListTL])
+        getTransactionLog(paramPage, paramDate, paramSearch,setListTL, setCountData, setLoading, history )
+      },[setListTL, paramPage, paramDate, paramSearch, history]);
 
     return ( 
-        // <div className="content-table-tl">
-        //     <div className="tl-title">Transaction List :</div>
-        //     <button>filter</button>
-        //     <div className="table-tl-list">
-        //         <DataTable 
-        //         columns={columns} 
-        //         data={listTL} 
-        //         size="middle"
-        //         pagePosition="bottomRight"/>
-        //     </div>   
-        // </div>   
-        
         <div className="transaction-log-constraint">
             <NavigationBar></NavigationBar>
             <div className="transaction-log-content">
                 <div className="tl-title">Transaction List :</div>
-                <button>filter</button>
+                <div className = "filter-search">
+                    <FilterBar  onChange={(date) => filterDate(date)} />
+                    <SearchBar 
+                    className="search-content"
+                    onSearch={(value) => searchTL(value)} />
+
+                </div>
+
+
                 <div className="table-tl-list">
-                    <Table 
+                    <DataTable 
                     columns={columns} 
                     data={listTL} 
-                    size="middle"
-                    pagePosition="bottomRight"/>
+                    pagePosition="bottomRight" 
+                    pageSize={20} 
+                    totalData={countData} 
+                    onPageChange={(page) => pageChange(page)}
+                    loading={loading}
+                    />
                 </div>  
             </div>
         </div>
